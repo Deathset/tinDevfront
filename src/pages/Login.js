cdf7a935c546b054cf6c71c4b9fe5fdeee7a4c5e
Diego Caiena <caienawork@outlook.com>
2019-08-07 14:38:43 -0300

    frontend

---
+++ src/pages/Login.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import './Login.css'
+import logo from '../../src/assets/logo.svg'
+import api from '../services/api'
+
+export default function Login({ history }) {
+    const [username, setUsername] = useState('')
+
+    async function handlesubmit(e) {
+        e.preventDefault()
+        
+        const resp = await api.post('/devs', { username, })
+        console.log(resp)
+
+        const { _id } = resp.data;
+
+
+        history.push(`/dev/${_id}`)
+    }
+
+
+    return (
+        <div className="login-container" >
+            <form onSubmit={handlesubmit} >
+                <img src={logo} alt="TindDev" />
+                <input
+                    placeholder="Digite o seu usuario do github"
+                    value={username}
+                    onChange={event => { setUsername(event.target.value) }}
+                />
+                <button type="submit" > Enviar</button>
+            </form>
+        </div>
+    );
+
+
+}
