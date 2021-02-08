100764aa49f16b5af4fce962bf93ff86b489348e
Joswells <wagnerjr300@gmail.com>
2021-02-08 20:07:38 -0300

    ajustes de alguns textos e indentação

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -13,13 +13,11 @@ import itsamatch from '../assets/match/itsamatch.png'
 export default function Main({ match }) {
 
     const [users, setUsers] = useState([])
-    const[matchDev, setMatchDev] = useState(null)
+    const [matchDev, setMatchDev] = useState(null)
     useEffect(() => {
         async function loadUsers() {
             const resp = await api.get('/devs', {
-                headers: {
-                    user: match.params.id
-                }
+                headers: { user: match.params.id }
             })
 
             setUsers(resp.data)
@@ -28,12 +26,10 @@ export default function Main({ match }) {
     }, [match.params.id])
 
     useEffect(() => {
-        const socket = io('http://localhost:3333',{
-            query:{ user: match.params.id}
+        const socket = io('http://localhost:3333', {
+            query: { user: match.params.id }
         })
-        socket.on('match', dev => {
-            setMatchDev(dev)
-        })
+        socket.on('match', dev => setMatchDev(dev))
     }, [match.params.id])
 
 
@@ -50,12 +46,12 @@ export default function Main({ match }) {
         setUsers(users.filter(user => user._id !== id))
     }
 
-   
+
     return (
         <div className="main_container" >
-         <Link to="/" >   
-            <img src={logo} alt="Tindev" />
-        </Link>
+            <Link to="/" >
+                <img src={logo} alt="Tindev" />
+            </Link>
             {users.length > 0 ? (
                 <ul>
                     {users.map((user) => (<li key={user._id}>
@@ -63,7 +59,7 @@ export default function Main({ match }) {
                         <footer>
                             <strong>{user.name}</strong>
                             <p>{user.bio}</p>
-                        </footer> 
+                        </footer>
 
                         <div className="buttons">
                             <button type="button" onClick={() => handledisLike(user._id)}>
@@ -75,21 +71,20 @@ export default function Main({ match }) {
                         </div>
                     </li>)
 
-                    )
-                    }
+                    )}
                 </ul>
-            ) :(
-                <div className="empty"> Acabou!!! :(</div>
-            ) }
+            ) : (
+                    <div className="empty"> Acabou!!! :(</div>
+                )}
 
             { matchDev && (
                 <div className="match_container">
                     <img src={itsamatch} alt="It's a match" />
-                    <img className="avatar" src={matchDev.avatar} alt="avatar"/>
+                    <img className="avatar" src={matchDev.avatar} alt="avatar" />
                     <strong> {matchDev.name} </strong>
                     <p>{matchDev.bio}</p>
-                    <button type='button' onClick ={() => setMatchDev(null)} >  Fechar! </button>
-                </div>   
+                    <button type='button' onClick={() => setMatchDev(null)}>Fechar!</button>
+                </div>
             )
 
             }
